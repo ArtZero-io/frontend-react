3d59a94a8ca1993cc818d099ac9bc15a894d0422
Nad128668 <nad128668@gmail.com>
2022-03-23 10:47:54 +0700

    more ArtZero NFT functions

--- src/pages/launchpad/index.js
+++ src/pages/launchpad/index.js
@@ -1,5 +1,5 @@
 import {
-  Button,
+  Button, Box, Flex, Text
 } from "@chakra-ui/react";
 import { useSubstrateState } from '../../utils/substrate'
 import Loader from '../../components/Loader/Loader'
@@ -16,11 +16,17 @@ const MintingEventPage = () => {
 
   const [whitelist,setWhitelist]  = useState(null);
   const [mintMode,setMintMode]    = useState(-1);
+  const [fee1,setFee1]            = useState(-1);
+  const [fee2,setFee2]            = useState(-1);
+  const [amount1,setAmount1]      = useState(-1);
 
   useEffect(async () => {
     dispatch(getProfile());
     await onGetWhiteList();
     await onGetMintMode();
+    await onGetFee1();
+    await onGetFee2();
+    await onGetAmount1();
   }, [dispatch, activeAddress]);
 
   const onGetWhiteList = async (e) => {
@@ -37,11 +43,37 @@ const MintingEventPage = () => {
     else
       setMintMode(-1);
   }
+  const onGetFee1 = async (e) => {
+    let res = await artzero_nft_calls.getFee1(currentAccount);
+    if (res)
+      setFee1(res);
+    else
+      setFee1(-1);
+  }
+  const onGetFee2 = async (e) => {
+    let res = await artzero_nft_calls.getFee2(currentAccount);
+    if (res)
+      setFee2(res);
+    else
+      setFee2(-1);
+  }
+  const onGetAmount1 = async (e) => {
+    let res = await artzero_nft_calls.getAmount1(currentAccount);
+    if (res)
+      setAmount1(res);
+    else
+      setAmount1(-1);
+  }
   const onWhiteListMint = async () => {
-
+    await artzero_nft_calls.whitelistMint(currentAccount,1);
+    await onGetWhiteList();
   }
   const onPaidMint = async () => {
-
+    if (mintMode == 1)
+      await artzero_nft_calls.paidMint(currentAccount,fee1);
+    else if (mintMode == 2)
+      await artzero_nft_calls.paidMint(currentAccount,fee2);
+    await onGetWhiteList();
   }
   return (
     <>
@@ -49,30 +81,65 @@ const MintingEventPage = () => {
         <Loader />
       ) : (
         <>
-        <Button
-          size="sm"
-          onClick={() => onGetWhiteList()}
-        >
-          get_whitelist
-        </Button>
-        <p>Your address: {activeAddress}</p>
-        <p>You are {whitelist ? "in the whitelist for minting ArtZero NFTs" : "not in the whitelist for minting ArtZero NFTs"}</p>
-        <p>{whitelist ? "You can claim " + whitelist.whitelistAmount + " ArtZero NFTs" : ""}</p>
-        <p>{whitelist ? "You already claimed " + whitelist.claimedAmount + " ArtZero NFTs" : ""}</p>
-        <p>------------</p>
-        <p>Minting is {mintMode <=0 ? "disabled" : "enabled"}</p>
-        <Button
-          size="sm"
-          onClick={() => onWhiteListMint()}
-        >
-          WhiteList Mint (FREE)
-        </Button>
-        <Button
-          size="sm"
-          onClick={() => onPaidMint()}
-        >
-          Mint Now (WITH FEE)
-        </Button>
+        <Box as="section" pt="20" pb="12" position="relative">
+          <Flex
+            position="relative"
+            direction="column"
+            align={{ sm: 'center' }}
+            maxW="2xl"
+            mx="auto"
+            color='white'
+            shadow={{ sm: 'base' }}
+            rounded={{ sm: 'lg' }}
+            px={{ base: '6', md: '8' }}
+            pb={{ base: '6', md: '8' }}
+          >
+            <Button
+              size="sm"
+              color='black'
+              onClick={() => onGetWhiteList()}
+            >
+              get_whitelist
+            </Button>
+            <Text>Your address: {activeAddress}</Text>
+            <Text>You are {whitelist ? "in the whitelist for minting ArtZero NFTs" : "not in the whitelist for minting ArtZero NFTs"}</Text>
+            <Text>{whitelist ? "You can claim " + whitelist.whitelistAmount + " ArtZero NFTs" : ""}</Text>
+            <Text>{whitelist ? "You already claimed " + whitelist.claimedAmount + " ArtZero NFTs" : ""}</Text>
+            <Text>------------</Text>
+            <Text>Minting is {mintMode <=0 ? "disabled" : "enabled"}</Text>
+            {mintMode == 1 ?
+                <>
+                  <Text>Minting fee is {fee1} AZERO</Text>
+                  <Text>Max Mint: {amount1} NFTs</Text>
+                </>
+              :
+              null
+            }
+            {mintMode == 2 ?
+                <>
+                  <Text>Minting fee is {fee2} AZERO</Text>
+                  <Text>Max Mint: {amount1} NFTs</Text>
+                </>
+              :
+              null
+            }
+            <Button
+              size="sm"
+              color='black'
+              onClick={() => onWhiteListMint()}
+            >
+              WhiteList Mint (FREE)
+            </Button>
+            <Button
+              size="sm"
+              color='black'
+              onClick={() => onPaidMint()}
+            >
+              Mint Now (WITH FEE)
+            </Button>
+          </Flex>
+
+          </Box>
         </>
       )}
     </>
--- src/utils/blockchain/artzero-nft-calls.js
+++ src/utils/blockchain/artzero-nft-calls.js
@@ -1,4 +1,7 @@
 //import { web3FromSource } from '@polkadot/extension-dapp'
+import BN from "bn.js";
+import toast from 'react-hot-toast'
+import { web3FromSource } from '@polkadot/extension-dapp'
 
 let artzero_contract
 
@@ -42,6 +45,181 @@ async function getMintMode(caller_account) {
   }
   return null;
 }
+async function getFee1(caller_account) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  const address = caller_account?.address
+  const gasLimit = -1
+  const azero_value = 0
+  console.log(artzero_contract);
+
+  const { result, output } = await artzero_contract.query.getFee1(
+    address,
+    { value:azero_value, gasLimit }
+  )
+  if (result.isOk) {
+
+    return new BN(output, 10, "le").toNumber()/ (10**12);
+  }
+  return null;
+}
+async function getFee2(caller_account) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  const address = caller_account?.address
+  const gasLimit = -1
+  const azero_value = 0
+  console.log(artzero_contract);
+
+  const { result, output } = await artzero_contract.query.getFee2(
+    address,
+    { value:azero_value, gasLimit }
+  )
+  if (result.isOk) {
+    return new BN(output, 10, "le").toNumber()/ (10**12);
+  }
+  return null;
+}
+async function getAmount1(caller_account) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  const address = caller_account?.address
+  const gasLimit = -1
+  const azero_value = 0
+  console.log(artzero_contract);
+
+  const { result, output } = await artzero_contract.query.getAmount1(
+    address,
+    { value:azero_value, gasLimit }
+  )
+  if (result.isOk) {
+    return new BN(output, 10, "le").toNumber();
+  }
+  return null;
+}
+
+async function tokenUri(caller_account) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  const address = caller_account?.address
+  const gasLimit = -1
+  const azero_value = 0
+
+  const { result, output } = await artzero_contract.query.tokenUri(
+    address,
+    { value:azero_value, gasLimit }
+  )
+  if (result.isOk) {
+    return output.toHuman();
+  }
+  return null;
+}
+
+async function whitelistMint(caller_account, mint_amount) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  let unsubscribe
+
+  const address = caller_account?.address;
+  const gasLimit = -1;
+  const azero_value = 0;
+  const injector = await web3FromSource(caller_account?.meta?.source)
+
+  artzero_contract.tx
+    .whitelistMint({ gasLimit, value:azero_value }, mint_amount)
+    .signAndSend(
+      address,
+      { signer: injector.signer },
+      async ({ status, dispatchError }) => {
+        if (dispatchError) {
+          if (dispatchError.isModule) {
+            const decoded = artzero_contract.registry.findMetaError(
+              dispatchError.asModule
+            )
+            const { docs, name, section } = decoded
+
+            console.log(`Lỗi: ${section}.${name}: ${docs.join(' ')}`)
+          } else {
+            console.log(
+              'dispatchError setProfileAttribute',
+              dispatchError.toString()
+            )
+          }
+        }
+
+        if (status) {
+          const statusText = Object.keys(status.toHuman())[0]
+          toast.success(
+            `whitelist minting ${
+              statusText === '0' ? 'started' : statusText.toLowerCase()
+            }.`
+          )
+        }
+      }
+    )
+    .then(unsub => (unsubscribe = unsub))
+    .catch(e => console.log('e', e));
+    return unsubscribe;
+}
+
+async function paidMint(caller_account, fee) {
+  if (!artzero_contract || !caller_account ){
+    console.log('invalid inputs');
+    return null;
+  }
+  let unsubscribe
+
+  const address = caller_account?.address;
+  const gasLimit = -1;
+  const azero_value = Math.round(fee * (10**12));
+  const injector = await web3FromSource(caller_account?.meta?.source)
+
+  artzero_contract.tx
+    .paidMint({ gasLimit, value:azero_value })
+    .signAndSend(
+      address,
+      { signer: injector.signer },
+      async ({ status, dispatchError }) => {
+        if (dispatchError) {
+          if (dispatchError.isModule) {
+            const decoded = artzero_contract.registry.findMetaError(
+              dispatchError.asModule
+            )
+            const { docs, name, section } = decoded
+
+            console.log(`Lỗi: ${section}.${name}: ${docs.join(' ')}`)
+          } else {
+            console.log(
+              'dispatchError setProfileAttribute',
+              dispatchError.toString()
+            )
+          }
+        }
+
+        if (status) {
+          const statusText = Object.keys(status.toHuman())[0]
+          toast.success(
+            `Public Minting ${
+              statusText === '0' ? 'started' : statusText.toLowerCase()
+            }.`
+          )
+        }
+      }
+    )
+    .then(unsub => (unsubscribe = unsub))
+    .catch(e => console.log('e', e));
+    return unsubscribe;
+}
 
 function setContract(c) {
   // console.log(`Setting contract in blockchain module`, c)
@@ -51,7 +229,13 @@ function setContract(c) {
 const artzero_contract_calls = {
   setContract,
   getWhitelist,
-  getMintMode
+  getMintMode,
+  getFee1,
+  getFee2,
+  getAmount1,
+  tokenUri,
+  whitelistMint,
+  paidMint
 }
 
 export default artzero_contract_calls
