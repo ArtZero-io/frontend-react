bb8b188d725abefecae519b8f63a03cfb298f63a
Nad128668 <nad128668@gmail.com>
2022-04-21 13:38:49 +0700

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,62 @@
 import {
   Center,
   Heading,
-  // Button,
-  // Flex,
-  // IconButton,
-  // Stack,
-  // Tag,
-  // TagLabel,
-  // TagLeftIcon,
-  // TagRightIcon,
-  // Text,
+  Button,
+  Flex,
+  IconButton,
+  Stack,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  TagRightIcon,
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../../components/Layout/Layout";
-// import { DownloadIcon } from "@chakra-ui/icons";
-// import AzeroIcon from "@theme/assets/icon/Azero.js";
-// import ActiveIcon from "@theme/assets/icon/Active.js";
-// import InActiveIcon from "@theme/assets/icon/InActive.js";
+import { DownloadIcon } from "@chakra-ui/icons";
+import AzeroIcon from "@theme/assets/icon/Azero.js";
+import ActiveIcon from "@theme/assets/icon/Active.js";
+import InActiveIcon from "@theme/assets/icon/InActive.js";
 
 function HomePage() {
   return (
     <Layout>
-      <Center h="80vh" w="full">
-        <Heading size="h1">Welcome to Artzero Demo</Heading>
+      <Center w="full">
+        <Heading size="h1">Artzero Smartnet Demo</Heading>
       </Center>
+      <Center w="full">
+        <Text fontSize="lg">Welcome to ArtZero - The first NFT Marketplace on Aleph Zero Network!</Text>
+        <Text fontSize="lg">We are proud to have the platform live on Smartnet. This is also one of the first dapps on AZero ecosystem.</Text>
+      </Center>
+      <Heading size="h6">How to Start?</Heading>
+      <br/>
+      <Text fontSize="lg">This version currently work with <a href="https://subwallet.app/" target="_blank">SubWallet</a>, <a href="https://polkadot.js.org/extension/" target="_blank">Polkadot JS</a> and <a href="https://chrome.google.com/webstore/detail/talisman-wallet/fijngjgcjhjmmpcmkeiomlglpeiijkld" target="_blank">Talisman</a> extensions. Please make sure you installed at least one of these wallets and create an account.</Text>
+      <br/>
+      <Text fontSize="lg">You will need some Smartnet AZero (SZERO) to start. Get free SZERO from <a href="https://faucet-smartnet.test.azero.dev/" target="_blank">https://faucet-smartnet.test.azero.dev/</a> </Text>
+      <br/>
+      <Center w="full">
+        <Text fontSize="lg">Introduction Video Here!</Text>
 
-      {/* <Flex w="full" justify="start" p={4}>
+      </Center>
+      <br/>
+      <Heading size="h6">Available Features</Heading>
+      <Text fontSize="lg">Support ERC721 (PSP34) Standard NFT</Text>
+      <Text fontSize="lg">Support Polkadot JS, SubWallet and Talisman Wallet</Text>
+      <Text fontSize="lg">On-chain Profile</Text>
+      <Text fontSize="lg">Explore NFT Collections</Text>
+      <Text fontSize="lg">Create Collections in Simple mode and Advanced mode</Text>
+      <Text fontSize="lg">Create NFT</Text>
+      <Text fontSize="lg">List, unlist, buy, bid NFTs</Text>
+      <Text fontSize="lg">Mint ArtZero NFTs on Smartnet (only 200 available)</Text>
+      <Text fontSize="lg">Stake ArtZero NFTs for trade discount</Text>
+      <br/>
+      <Heading size="h6">Up-coming Features</Heading>
+      <Text fontSize="lg">Support ERC1155 (PSP1155) Standard</Text>
+      <Text fontSize="lg">More documentation and tutorials</Text>
+      <Text fontSize="lg">Search for NFTs and Collections</Text>
+      <Text fontSize="lg">Launch Pad for NFT projects</Text>
+      <Text fontSize="lg"></Text>
+      <Flex w="full" justify="start" p={4}>
         <Stack p={10} bg="#464646">
           <Tag>
             <TagLeftIcon as={AzeroIcon} />
@@ -116,7 +147,7 @@ function HomePage() {
           <Heading size="h5">heading 5 24/30</Heading>
           <Heading size="h6">heading 6 18/22.5</Heading>
         </Stack>
-      </Flex> */}
+      </Flex>
     </Layout>
   );
 }
