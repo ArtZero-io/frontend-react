5d4acdf994466785ab48de547fe1f4e204fc7053
Albert Tran <alberttran0810@gmail.com>
2022-07-03 22:21:27 +0700

    Add new project form

--- src/pages/launchpad/component/GroupCard.js
+++ src/pages/launchpad/component/GroupCard.js
@@ -1,8 +1,47 @@
-import { Box, Button, Flex, Heading, Spacer } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+  Box
+} from "@chakra-ui/react";
+import ImageUpload from "@components/ImageUpload/Collection";
+import AdvancedModeInput from "@components/Input/Input";
+import { useEffect, useState } from "react";
 import React from "react";
 import { Card } from "./Card";
+import { isValidAddressPolkadotAddress } from "@utils";
+import launchpad_contract_calls from "@utils/blockchain/launchpad-contract-calls";
+import { Formik, Form } from "formik";
+import toast from "react-hot-toast";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { AccountActionTypes } from "@store/types/account.types";
+import { useSubstrateState } from "@utils/substrate";
 
 export const GroupCard = ({ variant = "live" }) => {
+  const [avatarIPFSUrl, setAvatarIPFSUrl] = useState("");
+  const dispatch = useDispatch();
+  const { currentAccount } = useSubstrateState();
+  const {
+    isOpen: isOpenAddNew,
+    onOpen: onOpenAddProject,
+    onClose: onCloseAddNewProject,
+  } = useDisclosure();
+  const { addCollectionTnxStatus } = useSelector(
+    (state) => state.account.accountLoaders
+  );
+  useEffect(() => {
+    addCollectionTnxStatus?.status === "End" && onCloseAddNewProject();
+  }, [onCloseAddNewProject, addCollectionTnxStatus?.status]);
+  
   return (
     <>
       <Box
@@ -31,7 +70,10 @@ export const GroupCard = ({ variant = "live" }) => {
 
           <Spacer />
 
-          {variant === "live" && <Button variant="solid">add project</Button>}
+          {variant === "live" && 
+            <Button 
+              variant="solid" 
+              onClick={() => onOpenAddProject()}>Add project</Button>}
         </Flex>
 
         <Flex justifyContent="space-between">
@@ -52,6 +94,99 @@ export const GroupCard = ({ variant = "live" }) => {
           <Button variant="outline">show more</Button>
         </Flex>
       </Box>
+
+      <Modal isCentered isOpen={isOpenAddNew} onClose={onCloseAddNewProject} size="xl">
+        <ModalOverlay
+          bg="blackAlpha.300"
+          backdropFilter="blur(10px) hue-rotate(90deg)"
+        />
+        <ModalContent
+          position="relative"
+          bg="brand.grayDark"
+          py={10}
+          px={20}
+          borderRadius="0"
+        >
+          <ModalCloseButton
+            position="absolute"
+            top="-8"
+            right="-8"
+            borderWidth={2}
+            borderRadius="0"
+          />
+          <ModalHeader textAlign="center">
+            <Heading size="h4" my={3}>
+              Add new project
+            </Heading>
+          </ModalHeader>
+
+          <ModalBody>
+            <Flex>
+            <Formik
+              validationSchema={Yup.object().shape({
+                projectName: Yup.string()
+                .min(3, "Must be longer than 3 characters")
+                .max(30, "Must be less than 30 characters")
+                .required("Required"),
+              })}
+              onSubmit={async (values, { setSubmitting }) => {
+                if (!avatarIPFSUrl) {
+                  return toast.error("Upload avatar");
+                }
+
+                // if (avatarIPFSUrl && headerIPFSUrl && headerSquareIPFSUrl) {
+                values.avatarIPFSUrl = avatarIPFSUrl;
+
+
+                if (!isValidAddressPolkadotAddress(values.nftContractAddress)) {
+                  toast.error(`The NFT contract address must be an address!`);
+                } else {
+                  const data = {
+
+                  };
+
+                  dispatch({
+                    type: AccountActionTypes.SET_ADD_COLLECTION_TNX_STATUS,
+                    payload: {
+                      status: "Start",
+                    },
+                  });
+
+                  await launchpad_contract_calls.addNewProject(
+                    currentAccount,
+                    data,
+                    dispatch
+                  );
+                 
+                }
+                // }
+              }}
+            >
+              {({ values, dirty, isValid }) => (
+                <Form>
+                  <AdvancedModeInput
+                    isRequired={true}
+                    label="Project Name"
+                    name="projectName"
+                    type="projectName"
+                    placeholder="Project Name"
+                  />
+                  <ImageUpload
+                      isDisabled={addCollectionTnxStatus}
+                      id="project-avatar"
+                      imageIPFSUrl={avatarIPFSUrl}
+                      setImageIPFSUrl={setAvatarIPFSUrl}
+                      title="Project Avatar Image"
+                      limitedSize={{ width: "100", height: "100" }}
+                    />
+                </Form>
+              )}
+            </Formik>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
     </>
   );
 };
---
+++ src/utils/blockchain/launchpad-contract-calls.js
@@ -0,0 +1,112 @@
+import toast from "react-hot-toast";
+import { web3FromSource } from "../wallets/extension-dapp";
+import {
+  handleContractCallAnimation,
+} from "@utils";
+import { ContractPromise } from "@polkadot/api-contract";
+import { clientAPI } from "@api/client";
+import { AccountActionTypes } from "@store/types/account.types";
+
+let contract;
+
+export const setLaunchPadContract = (api, data) => {
+  contract = new ContractPromise(
+    api,
+    data?.CONTRACT_ABI,
+    data?.CONTRACT_ADDRESS
+  );
+};
+
+async function getAttributes(caller_account, collection_address, attributes) {
+  if (!contract || !caller_account) {
+    return null;
+  }
+  let attributeVals;
+  const gasLimit = -1;
+  const azero_value = 0;
+  const address = caller_account?.address;
+  const { result, output } = await contract.query.getAttributes(
+    address,
+    { value: azero_value, gasLimit },
+    collection_address,
+    attributes
+  );
+  if (result.isOk) {
+    attributeVals = output.toHuman();
+  }
+  return attributeVals;
+}
+
+async function setMultipleAttributes(
+  account,
+  collection_address,
+  attributes,
+  values,
+  dispatch
+) {
+  let unsubscribe;
+
+  const address = account?.address;
+  const gasLimit = -1;
+  const value = 0;
+
+  const injector = await web3FromSource(account?.meta?.source);
+
+  account &&
+    contract.tx
+      .setMultipleAttributes(
+        { gasLimit, value },
+        collection_address,
+        attributes,
+        values
+      )
+      .signAndSend(
+        address,
+        { signer: injector.signer },
+        async ({ status, dispatchError }) => {
+          if (dispatchError) {
+            if (dispatchError.isModule) {
+              toast.error(`There is some error with your request`);
+            } else {
+              console.log("dispatchError ", dispatchError.toString());
+            }
+          }
+
+          if (status) {
+            handleContractCallAnimation(status, dispatchError, dispatch);
+
+            if (status.isFinalized === true) {
+              await clientAPI("post", "/updateCollection", {
+                collection_address: collection_address,
+              });
+            }
+
+            // const statusText = Object.keys(status.toHuman())[0];
+            // toast.success(
+            //   `Update Collection Attributes ${
+            //     statusText === "0" ? "started" : statusText.toLowerCase()
+            //   }.`
+            // );
+          }
+        }
+      )
+      .then((unsub) => (unsubscribe = unsub))
+      .catch((e) => {
+        dispatch({
+          type: AccountActionTypes.CLEAR_ADD_COLLECTION_TNX_STATUS,
+        });
+        const mess = `Tnx is ${e.message}`;
+
+        toast.error(mess);
+      });
+
+  return unsubscribe;
+}
+
+const launchpad_contract_calls = {
+  setLaunchPadContract,
+  getAttributes,
+  setMultipleAttributes
+};
+
+export default launchpad_contract_calls;
