5ebe2c7146f048733a3cefd656ebf6ee97dea148
j <langdon0003@gmail.com>
2022-09-29 09:33:58 +0700

    add console log info Tnx fee

--- src/store/actions/txStatus.js
+++ src/store/actions/txStatus.js
@@ -1,7 +1,7 @@
 import toast from "react-hot-toast";
 import { SET_STATUS, CLEAR_STATUS } from "../types/txStatus";
 import { READY, FINALIZED } from "@constants";
-import { fetchUserBalance } from "../../pages/launchpad/component/Form/AddNewProject";
+// import { fetchUserBalance } from "../../pages/launchpad/component/Form/AddNewProject";
 
 export const setTxStatus = (props) => {
   return (dispatch) => {
@@ -51,8 +51,64 @@ export const txResponseErrorHandler = async ({
     if (dispatchError.isModule) {
       const decoded = api.registry.findMetaError(dispatchError.asModule);
       const { docs, name, section } = decoded;
-      console.log("section", `${section}.${name}: ${docs.join(" ")}`);
+
+      const statusToHuman = Object.entries(status.toHuman());
+
+      const url = `https://test.azero.dev/#/explorer/query/`;
+
+      console.log("^^ Error\t\t\t\t:", `${section}.${name}: ${docs.join(" ")}`);
+
+      const apiAt = await api.at(statusToHuman[0][1]);
+      const allEventsRecords = await apiAt.query.system.events();
+
+      allEventsRecords.forEach(({ event }, index) => {
+        if (api.events.transactionPayment?.TransactionFeePaid.is(event)) {
+          console.log(
+            "^^ Txn Fee Paid\t\t\t:   -",
+            (
+              parseInt(event.data[1].toHuman().replaceAll(",", "")) /
+              10 ** 12
+            ).toFixed(6)
+          );
+        }
+
+        if (api.events.balances?.Reserved.is(event)) {
+          console.log(
+            "^^ Reserved\t\t\t\t:   -",
+            (
+              parseInt(event.data[1].toHuman().replaceAll(",", "")) /
+              10 ** 12
+            ).toFixed(6)
+          );
+        }
+
+        if (api.events.balances?.ReserveRepatriated.is(event)) {
+          console.log(
+            "^^ Reserve Repatriated\t:   +",
+            (
+              parseInt(event.data[2].toHuman().replaceAll(",", "")) /
+              10 ** 12
+            ).toFixed(6)
+          );
+        }
+      });
+
+      const { data: balance } = await api.query.system.account(
+        caller_account?.address
+      );
+
+      console.log(
+        "^^ Balance END\t\t\t:",
+        balance.free.toHuman().slice(0, -16) +
+          "." +
+          balance.free.toHuman().slice(-15, -8)
+      );
+
+      console.log("^^ Tx finalized at ", `${url}${statusToHuman[0][1]}`);
+      console.log(`^^==================Log end==================`);
+
       toast.error(`There is some error with your request..`);
+      // console.log("section", `${section}.${name}: ${docs.join(" ")}`);
       // return toast.error(`${section}.${name}: ${docs.join(" ")}`);
     } else {
       // console.log("dispatchError.toString()", dispatchError.toString());
@@ -68,8 +124,25 @@ export const txResponseErrorHandler = async ({
     if (Object.keys(status.toHuman())[0] === "0") {
       dispatch(setTxStatus({ txType, txStatus: READY, step: READY, type }));
 
-      await fetchUserBalance({ currentAccount: caller_account, api }).then(
-        ({ balance }) => console.log(txType, " bal ready: ", balance)
+      // await fetchUserBalance({ currentAccount: caller_account, api }).then(
+      //   ({ balance }) =>
+      //     console.log("===ActionType:", txType, ". Balance START: ", balance)
+      // );
+
+      const now = await api.query.timestamp.now();
+      console.log(`^^\n`);
+      console.log(`^^================Log start==================`);
+
+      console.log(`^^ Log time\t\t\t\t: ${new Date(Number(now))}`);
+      console.log(`^^ ActionType\t\t\t: ${txType}`);
+      const { data: balance } = await api.query.system.account(
+        caller_account?.address
+      );
+      console.log(
+        "^^ Balance START\t\t:",
+        balance.free.toHuman().slice(0, -16) +
+          "." +
+          balance.free.toHuman().slice(-15, -8)
       );
     } else {
       dispatch(
@@ -83,11 +156,64 @@ export const txResponseErrorHandler = async ({
       );
 
       if (statusToHuman[0][0] === FINALIZED) {
-        await fetchUserBalance({ currentAccount: caller_account, api }).then(
-          ({ balance }) => console.log(txType, " bal final: ", balance)
+        // await fetchUserBalance({ currentAccount: caller_account, api }).then(
+        //   ({ balance }) =>
+        //     console.log("===ActionType:", txType, "Balance END: ", balance)
+        // );
+
+        const apiAt = await api.at(statusToHuman[0][1]);
+        const allEventsRecords = await apiAt.query.system.events();
+
+        allEventsRecords.forEach(({ event }, index) => {
+          // if (api.events.balances?.Transfer.is(event)) {
+          //   console.log("===0Transfer From\t\t\t\t: -", event.data[0].toHuman());
+          //   console.log("===1Transfer To\t\t\t\t: -", event.data[1].toHuman());
+          //   console.log("===2Transfer Amount\t\t\t\t: -", event.data[2].toHuman());
+          // }
+
+          if (api.events.transactionPayment?.TransactionFeePaid.is(event)) {
+            console.log(
+              "^^ Txn Fee Paid\t\t\t:   -",
+              (
+                parseInt(event.data[1].toHuman().replaceAll(",", "")) /
+                10 ** 12
+              ).toFixed(6)
+            );
+          }
+
+          if (api.events.balances?.Reserved.is(event)) {
+            console.log(
+              "^^ Reserved\t\t\t\t:   -",
+              (
+                parseInt(event.data[1].toHuman().replaceAll(",", "")) /
+                10 ** 12
+              ).toFixed(6)
+            );
+          }
+
+          if (api.events.balances?.ReserveRepatriated.is(event)) {
+            console.log(
+              "^^ Reserve Repatriated\t:   +",
+              (
+                parseInt(event.data[2].toHuman().replaceAll(",", "")) /
+                10 ** 12
+              ).toFixed(6)
+            );
+          }
+        });
+
+        const { data: balance } = await api.query.system.account(
+          caller_account?.address
         );
 
-        console.log("Tx finalized at ", `${url}${statusToHuman[0][1]}`);
+        console.log(
+          "^^ Balance END\t\t\t:",
+          balance.free.toHuman().slice(0, -16) +
+            "." +
+            balance.free.toHuman().slice(-15, -8)
+        );
+        console.log("^^ Tx finalized at ", `${url}${statusToHuman[0][1]}`);
+        console.log(`^^==================Log end==================`);
       }
     }
   }
