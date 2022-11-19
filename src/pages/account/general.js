f948a5de462df599fd58444a090ded8a0142e10a
j <langdon0003@gmail.com>
2022-11-19 15:44:57 +0700

    My General Page: update catch error

--- src/pages/account/general.js
+++ src/pages/account/general.js
@@ -27,36 +27,37 @@ import {
   Center,
   Td,
   Skeleton,
-} from "@chakra-ui/react";
-import { useHistory } from "react-router-dom";
+} from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 
-import * as ROUTES from "@constants/routes";
-import AzeroIcon from "@theme/assets/icon/Azero.js";
+import * as ROUTES from '@constants/routes';
+import AzeroIcon from '@theme/assets/icon/Azero.js';
 
-import FeeInfoModal from "./components/Modal/FeeInfo";
+import FeeInfoModal from './components/Modal/FeeInfo';
 
-import { useCallback, useEffect, useState } from "react";
-import { useSubstrateState } from "@utils/substrate";
-import toast from "react-hot-toast";
+import { useCallback, useEffect, useState } from 'react';
+import { useSubstrateState } from '@utils/substrate';
+import toast from 'react-hot-toast';
 
-import staking_calls from "@utils/blockchain/staking_calls";
-import marketplace_contract_calls from "@utils/blockchain/marketplace_contract_calls";
-import BN from "bn.js";
-import { motion } from "framer-motion";
-import { truncateStr } from "@utils";
-import CommonContainer from "../../components/Container/CommonContainer";
-import { fetchUserBalance } from "../launchpad/component/Form/AddNewProject";
-import launchpad_manager from "@utils/blockchain/launchpad-manager";
-import collection_manager from "@utils/blockchain/collection-manager";
-import useTxStatus from "@hooks/useTxStatus";
-import CommonButton from "@components/Button/CommonButton";
-import { useDispatch } from "react-redux";
-import { START, CLAIM_REWARDS } from "@constants";
-import { setTxStatus } from "@store/actions/txStatus";
-import useForceUpdate from "@hooks/useForceUpdate";
+import staking_calls from '@utils/blockchain/staking_calls';
+import marketplace_contract_calls from '@utils/blockchain/marketplace_contract_calls';
+import BN from 'bn.js';
+import { motion } from 'framer-motion';
+import { truncateStr } from '@utils';
+import CommonContainer from '../../components/Container/CommonContainer';
+import { fetchUserBalance } from '../launchpad/component/Form/AddNewProject';
+import launchpad_manager from '@utils/blockchain/launchpad-manager';
+import collection_manager from '@utils/blockchain/collection-manager';
+import useTxStatus from '@hooks/useTxStatus';
+import CommonButton from '@components/Button/CommonButton';
+import { useDispatch } from 'react-redux';
+import { START, CLAIM_REWARDS } from '@constants';
+import { setTxStatus } from '@store/actions/txStatus';
+import useForceUpdate from '@hooks/useForceUpdate';
 
-import { APICall } from "@api/client";
-import { useMemo } from "react";
+import { APICall } from '@api/client';
+import { useMemo } from 'react';
+import { clearTxStatus } from '@store/actions/txStatus';
 
 function GeneralPage() {
   const history = useHistory();
@@ -162,10 +163,10 @@ function GeneralPage() {
 
           info = [
             ...info,
-            { name: "NFTs for sale", value: nftForSale },
-            { name: "Staked NFTs", value: totalStaked },
+            { name: 'NFTs for sale', value: nftForSale },
+            { name: 'Staked NFTs', value: totalStaked },
             {
-              name: "Total Owned NFTs",
+              name: 'Total Owned NFTs',
               value: nftList.length + totalStaked,
             },
           ];
@@ -179,7 +180,7 @@ function GeneralPage() {
     } catch (error) {
       console.log(error);
 
-      toast.error("There was an error while fetching the collections.");
+      toast.error('There was an error while fetching the collections.');
     }
   }, [api, currentAccount]);
 
@@ -220,18 +221,24 @@ function GeneralPage() {
   }, [currentAccount]);
 
   const claimReward = async () => {
-    dispatch(setTxStatus({ type: CLAIM_REWARDS, step: START }));
+    try {
+      dispatch(setTxStatus({ type: CLAIM_REWARDS, step: START }));
 
-    await staking_calls.claimReward(
-      currentAccount,
-      dispatch,
-      CLAIM_REWARDS,
-      api
-    );
-    await checkRewardStatus();
+      await staking_calls.claimReward(
+        currentAccount,
+        dispatch,
+        CLAIM_REWARDS,
+        api
+      );
+      await checkRewardStatus();
+    } catch (error) {
+      console.log(error);
+      toast.error('There was an error while claiming the rewards.');
+      dispatch(clearTxStatus());
+    }
   };
 
-  const [isLargerThan480] = useMediaQuery("(min-width: 480px)");
+  const [isLargerThan480] = useMediaQuery('(min-width: 480px)');
   const { tokenIDArray, actionType, ...rest } = useTxStatus();
 
   useEffect(() => {
@@ -266,25 +273,24 @@ function GeneralPage() {
     const now = new Date();
     const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
 
-    return lastDay.toLocaleString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
+    return lastDay.toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
     });
   }, []);
-
 
   return (
     <CommonContainer>
       <>
         <VStack as="section" w="full">
-          <Box w="full" textAlign="left" mb={[0, "24px"]}>
+          <Box w="full" textAlign="left" mb={[0, '24px']}>
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
             >
-              <Heading fontSize={["3xl-mid", "5xl", "5xl"]}>dashboard</Heading>
+              <Heading fontSize={['3xl-mid', '5xl', '5xl']}>dashboard</Heading>
 
               <Flex alignItems="center">
                 <Button
@@ -297,7 +303,7 @@ function GeneralPage() {
                   mr={3}
                   h={9}
                 >
-                  <Text fontFamily="Oswald" fontSize={["14px", "16px"]}>
+                  <Text fontFamily="Oswald" fontSize={['14px', '16px']}>
                     {isLargerThan480
                       ? currentAccount?.address
                       : truncateStr(currentAccount?.address, 16)}
@@ -308,7 +314,7 @@ function GeneralPage() {
                     px={2}
                     h={8}
                     mx={0}
-                    minW={"sm"}
+                    minW={'sm'}
                     readOnly={true}
                     cursor="pointer"
                     color="brand.blue"
@@ -317,30 +323,30 @@ function GeneralPage() {
                   />
                 </Button>
                 {hasCopied ? (
-                  <Tag variant="outline" fontSize={["14px", "16px"]}>
+                  <Tag variant="outline" fontSize={['14px', '16px']}>
                     <TagLabel>Copied</TagLabel>
                   </Tag>
                 ) : (
-                  ""
+                  ''
                 )}
                 <Spacer />
               </Flex>
             </motion.div>
           </Box>
 
-          <div style={{ display: "grid", width: "100%" }}>
+          <div style={{ display: 'grid', width: '100%' }}>
             <Grid
               w="full"
-              minH={"7rem"}
-              gap={{ base: "10px", md: "30px" }}
+              minH={'7rem'}
+              gap={{ base: '10px', md: '30px' }}
               templateColumns={{
-                base: "repeat(auto-fill, minmax(min(100%, 200px), 1fr))",
-                lg: "repeat(auto-fill, minmax(min(100%, 290px), 1fr))",
-                xl: "repeat(auto-fill, minmax(min(100%, 320px), 1fr))",
+                base: 'repeat(auto-fill, minmax(min(100%, 200px), 1fr))',
+                lg: 'repeat(auto-fill, minmax(min(100%, 290px), 1fr))',
+                xl: 'repeat(auto-fill, minmax(min(100%, 320px), 1fr))',
               }}
             >
               {dashboardInfo
-                ?.filter((item) => !Object.keys(item).includes("address"))
+                ?.filter((item) => !Object.keys(item).includes('address'))
                 .map((item, idx) => {
                   return (
                     <GridItem key={idx} w="100%" h="100%">
@@ -360,8 +366,8 @@ function GeneralPage() {
                             <Flex w="full">
                               <Box>
                                 <Text
-                                  display={["none", "block"]}
-                                  fontSize={{ base: "md", md: "lg" }}
+                                  display={['none', 'block']}
+                                  fontSize={{ base: 'md', md: 'lg' }}
                                   color="#888"
                                 >
                                   {item.name}
@@ -371,7 +377,7 @@ function GeneralPage() {
                             </Flex>
                             <Flex w="full" textAlign="left" alignItems="center">
                               <Text
-                                display={["block", "none"]}
+                                display={['block', 'none']}
                                 fontSize="lg"
                                 color="#888"
                               >
@@ -382,11 +388,11 @@ function GeneralPage() {
                                 <Tag bg="transparent" pl={0}>
                                   <TagLabel
                                     bg="transparent"
-                                    fontSize={["24px", "40px"]}
+                                    fontSize={['24px', '40px']}
                                   >
                                     {item.value}
                                   </TagLabel>
-                                  {item.name === "Amount Trades" && (
+                                  {item.name === 'Amount Trades' && (
                                     <TagRightIcon
                                       fontSize="2xl"
                                       as={AzeroIcon}
@@ -411,8 +417,8 @@ function GeneralPage() {
           bg="black"
           pos="relative"
           maxW="container.xl"
-          p={{ base: "20px", md: "30px" }}
-          direction={{ base: "column", md: "row" }}
+          p={{ base: '20px', md: '30px' }}
+          direction={{ base: 'column', md: 'row' }}
         >
           <Square
             mx="auto"
@@ -429,7 +435,7 @@ function GeneralPage() {
             />
           </Square>
 
-          <Flex w="full" display={["flex", "none"]} justifyContent="start">
+          <Flex w="full" display={['flex', 'none']} justifyContent="start">
             <Tag variant="outline" my="10px">
               <TagLabel>Your Trade Fee: {tradeFee && `${tradeFee}%`}</TagLabel>
             </Tag>
@@ -437,34 +443,34 @@ function GeneralPage() {
 
           <VStack
             w="full"
-            pr={[0, "8px"]}
+            pr={[0, '8px']}
             textAlign="left"
-            h={{ xl: "288px" }}
-            pl={{ base: "0", lg: "10px", xl: "40px" }}
+            h={{ xl: '288px' }}
+            pl={{ base: '0', lg: '10px', xl: '40px' }}
             spacing="10px"
           >
             <Flex w="full" mb="8px">
               <Box
                 w="full"
-                lineHeight={["30px", "48px"]}
+                lineHeight={['30px', '48px']}
                 fontFamily="Evogria Italic"
-                fontSize={{ base: "24px", md: "28px", xl: "3xl-mid" }}
+                fontSize={{ base: '24px', md: '28px', xl: '3xl-mid' }}
               >
                 <span>stake your </span>
-                <span style={{ color: "#7AE7FF" }}>
+                <span style={{ color: '#7AE7FF' }}>
                   praying mantis predators
-                </span>{" "}
-                <Box display={{ base: "none", xl: "flex" }} />
+                </span>{' '}
+                <Box display={{ base: 'none', xl: 'flex' }} />
                 <span>to reduce your </span>
-                <span style={{ color: "#7AE7FF" }}>fees</span>
+                <span style={{ color: '#7AE7FF' }}>fees</span>
                 <span> and earn </span>
-                <span style={{ color: "#7AE7FF" }}>AZERO</span>
+                <span style={{ color: '#7AE7FF' }}>AZERO</span>
               </Box>
 
               <Spacer />
 
-              <Box variant="outline" display={{ base: "none", md: "flex" }}>
-                <Tag variant="outline" h={6} minW={"128px"} mt={3}>
+              <Box variant="outline" display={{ base: 'none', md: 'flex' }}>
+                <Tag variant="outline" h={6} minW={'128px'} mt={3}>
                   {
                     <TagLabel fontSize="16px">
                       Your Trade Fee: {tradeFee && `${tradeFee}%`}
@@ -477,21 +483,21 @@ function GeneralPage() {
             <Stack
               w="full"
               spacing="8px"
-              direction={{ base: "column", md: "row" }}
+              direction={{ base: 'column', md: 'row' }}
             >
               <Stack minW="280px">
-                <Text color="#fff" fontSize={{ base: "16px", xl: "lg" }}>
-                  Your Total Stake:{" "}
-                  <span style={{ color: "#7AE7FF" }}>
-                    {totalStaked || 0} NFT{totalStaked > 1 ? "s" : ""}
+                <Text color="#fff" fontSize={{ base: '16px', xl: 'lg' }}>
+                  Your Total Stake:{' '}
+                  <span style={{ color: '#7AE7FF' }}>
+                    {totalStaked || 0} NFT{totalStaked > 1 ? 's' : ''}
                   </span>
                 </Text>
               </Stack>
               <Stack>
-                <Text fontSize={{ base: "16px", xl: "lg" }}>
-                  Reward Distribution Status:{" "}
+                <Text fontSize={{ base: '16px', xl: 'lg' }}>
+                  Reward Distribution Status:{' '}
                   <Text as="span" color="#7AE7FF" mr="30px">
-                    {rewardStarted ? "Active" : "Inactive"}
+                    {rewardStarted ? 'Active' : 'Inactive'}
                   </Text>
                 </Text>
               </Stack>
@@ -500,11 +506,11 @@ function GeneralPage() {
             <Stack
               w="full"
               spacing="8px"
-              direction={{ base: "column", md: "row" }}
+              direction={{ base: 'column', md: 'row' }}
             >
               <Skeleton minW="280px" isLoaded={!loading}>
-                <Text color="#fff" fontSize={{ base: "16px", xl: "lg" }}>
-                  Your Estimated Earning:{" "}
+                <Text color="#fff" fontSize={{ base: '16px', xl: 'lg' }}>
+                  Your Estimated Earning:{' '}
                   <Text as="span" color="#7AE7FF" mr="30px">
                     {rewardStarted && claimed
                       ? 0
@@ -512,19 +518,19 @@ function GeneralPage() {
                       ? parseFloat(estimatedEarning).toFixed(3)
                       : parseFloat(estimatedEarningBaseRewardPool).toFixed(
                           3
-                        )}{" "}
+                        )}{' '}
                     <AzeroIcon
                       mb="2px"
-                      w={["14px", "16px"]}
-                      h={["14px", "16px"]}
+                      w={['14px', '16px']}
+                      h={['14px', '16px']}
                     />
                   </Text>
                 </Text>
               </Skeleton>
 
               <Stack>
-                <Text fontSize={{ base: "16px", xl: "lg" }}>
-                  Next Payout:{" "}
+                <Text fontSize={{ base: '16px', xl: 'lg' }}>
+                  Next Payout:{' '}
                   <Text as="span" color="#7AE7FF" mr="30px">
                     {lastDay}
                   </Text>
@@ -535,15 +541,15 @@ function GeneralPage() {
             <Skeleton
               h="full"
               w="full"
-              pt={["10px", 0]}
+              pt={['10px', 0]}
               display="flex"
               isLoaded={!loading}
-              alignItems={["start", "center"]}
-              flexDirection={{ base: "column", md: "row" }}
+              alignItems={['start', 'center']}
+              flexDirection={{ base: 'column', md: 'row' }}
             >
               <Button
                 variant="solid"
-                w={{ base: "full", md: "auto" }}
+                w={{ base: 'full', md: 'auto' }}
                 onClick={() => history.push(ROUTES.ACCOUNT_MY_STAKES)}
               >
                 stake now
@@ -554,10 +560,10 @@ function GeneralPage() {
                   {...rest}
                   w="full"
                   minW="120px"
-                  mx={["0", "30px"]}
-                  my={["20px", "30px"]}
-                  maxW={["auto", "220px"]}
-                  text={claimed ? "rewards is claimed" : "claim rewards"}
+                  mx={['0', '30px']}
+                  my={['20px', '30px']}
+                  maxW={['auto', '220px']}
+                  text={claimed ? 'rewards is claimed' : 'claim rewards'}
                   isDisabled={claimed || !estimatedEarningBaseRewardPool}
                   onClick={() => claimReward()}
                 />
@@ -572,7 +578,7 @@ function GeneralPage() {
           <Box maxW="6xl-mid" mt="30px">
             <HStack pb={5} borderBottomWidth={1}>
               <Flex alignItems="start" pr={20}>
-                <Heading fontSize={"18px"} ml={1}>
+                <Heading fontSize={'18px'} ml={1}>
                   Recent Reward History
                 </Heading>
               </Flex>
@@ -584,19 +590,19 @@ function GeneralPage() {
               h="full"
               overflow="auto"
               sx={{
-                "&::-webkit-scrollbar": {
-                  width: "4px",
-                  height: "4px",
-                  borderRadius: "0px",
+                '&::-webkit-scrollbar': {
+                  width: '4px',
+                  height: '4px',
+                  borderRadius: '0px',
                   backgroundColor: `transparent`,
                 },
-                "&::-webkit-scrollbar-thumb": {
+                '&::-webkit-scrollbar-thumb': {
                   backgroundColor: `#7ae7ff`,
                 },
-                "&::-webkit-scrollbar-thumb:hover": {
+                '&::-webkit-scrollbar-thumb:hover': {
                   backgroundColor: `#7ae7ff`,
                 },
-                "&::-webkit-scrollbar-track": {
+                '&::-webkit-scrollbar-track': {
                   backgroundColor: `transparent`,
                 },
               }}
@@ -664,14 +670,14 @@ function GeneralPage() {
                           </Td>
                           <Td textAlign="left" py={7}>
                             {reward.stakedAmount} NFT
-                            {reward.stakedAmount > 1 ? "s" : ""}
+                            {reward.stakedAmount > 1 ? 's' : ''}
                           </Td>
                           <Td textAlign="left" py={7}>
-                            {reward.rewardAmount.toFixed(3)}{" "}
+                            {reward.rewardAmount.toFixed(3)}{' '}
                             <AzeroIcon
                               mb="4px"
-                              w={["14px", "16px"]}
-                              h={["14px", "16px"]}
+                              w={['14px', '16px']}
+                              h={['14px', '16px']}
                             />
                           </Td>
                         </Tr>
