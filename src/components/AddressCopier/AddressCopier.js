086db0ad067b140a22a4700a2f1830f5de4499bb
thiennv <thien09062000@gmail.com>
2023-04-06 09:51:31 +0700

    merge branch mainnet

--- src/components/AddressCopier/AddressCopier.js
+++ src/components/AddressCopier/AddressCopier.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useClipboard, Flex, Text } from "@chakra-ui/react";
+import { useClipboard, Flex } from "@chakra-ui/react";
 import { truncateStr } from "@utils";
 import toast from "react-hot-toast";
 import { CopyIcon } from "@chakra-ui/icons";
@@ -8,7 +8,6 @@ export default function AddressCopier({
   address,
   truncateStrNum = 5,
   hasIcon = false,
-  textOnly = false,
 }) {
   const { onCopy } = useClipboard(address);
 
@@ -19,21 +18,15 @@ export default function AddressCopier({
 
   return (
     <>
-      {textOnly ? (
-        <Text as="span" cursor="pointer" _hover={{ color: "#7ae7ff" }} onClick={handleCopy}>
-          {truncateStr(address, truncateStrNum)}{" "}
-        </Text>
-      ) : (
-        <Flex
-          cursor="pointer"
-          _hover={{ color: "#7ae7ff" }}
-          onClick={handleCopy}
-          alignItems="center"
-        >
-          {truncateStr(address, truncateStrNum)}{" "}
-          {address && hasIcon && <CopyIcon ml="8px" />}
-        </Flex>
-      )}
+      <Flex
+        cursor="pointer"
+        _hover={{ color: "#7ae7ff" }}
+        onClick={handleCopy}
+        alignItems="center"
+      >
+        {truncateStr(address, truncateStrNum)}{" "}
+        {address && hasIcon && <CopyIcon ml="8px" />}
+      </Flex>
     </>
   );
 }
