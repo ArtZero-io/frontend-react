e70352a4d326a9603d73025195cd6688ed2a00dc
j <langdon0003@gmail.com>
2023-08-24 14:16:21 +0700

    Add toast loading wallet account

--- src/utils/substrate/SubstrateContext.js
+++ src/utils/substrate/SubstrateContext.js
@@ -10,6 +10,7 @@ import { TypeRegistry } from "@polkadot/types/create";
 import config from "./config";
 // eslint-disable-next-line no-unused-vars
 import { resolveDomain } from "..";
+import { toast } from "react-hot-toast";
 // import { toast } from "react-hot-toast";
 
 const parsedQuery = new URLSearchParams(window.location.search);
@@ -157,6 +158,7 @@ export const loadAccounts = async (state, dispatch, wallet) => {
   const asyncLoadAccounts = async () => {
     try {
       await web3Enable(config.APP_NAME, [], wallet);
+      console.log("web3Enable...");
 
       let allAccounts = await web3Accounts();
 
@@ -166,11 +168,11 @@ export const loadAccounts = async (state, dispatch, wallet) => {
       }));
 
       try {
-        // toast(`Loading ${allAccounts?.length} accounts domain...`, {
-        //   style: {
-        //     minWidth: "180px",
-        //   },
-        // });
+        toast.success(
+          `Loading ${allAccounts?.length} account${
+            allAccounts?.length > 1 ? "s" : ""
+          } ...`
+        );
 
         allAccounts = await Promise.all(
           allAccounts.map(async (item) => {
